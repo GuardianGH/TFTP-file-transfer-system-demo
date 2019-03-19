df83f356aff23a5a8523265ba72440b1a38af248
GuardianGH <39890235+GuardianGH@users.noreply.github.com>
2019-03-19 11:12:22 +0800

    Add files via upload

---
+++ 183-TFTP_client_并发服务器客户端.py
@@ -0,0 +1,110 @@
+#coding=utf-8
+from socket import socket
+import sys
+import os
+import time
+import tkinter.filedialog as flog
+
+class Tftpclient():
+    def __init__(self,sockfd):
+        self.sockfd = sockfd
+    def do_liebiao(self):
+        self.sockfd.send('list'.encode())
+        
+        # 服务器回复y/n
+        data = self.sockfd.recv(1024).decode()
+        if data == 'y':
+            data = self.sockfd.recv(4096).decode()
+            files = data.split(',')
+            print('文件列表如下')
+            for f in files:
+                print(f)
+            print('文件列表展示完毕')
+        else:
+            print('请求失败')
+        
+    def do_xiazai(self, filename):
+        self.sockfd.send(('G ' + filename).encode())
+        data = self.sockfd.recv(1024).decode()
+        if data == 'y':
+            fd = open(filename, 'wb')
+            while 1:
+                fdata = self.sockfd.recv(1024)
+
+                if fdata == '##':
+                    break
+
+                fd.write(fdata)
+            fd.close()
+            print('%s　下载完成'%filename)
+        else:
+            print('服务器拒绝下载')
+    def do_shangchuan(self,filename):
+        try:
+            filename = flog.askopenfilename(titel = '选择上传的文件')
+            fd = open(filename,'rb')
+        except:
+            print('出错')
+            return
+        fname = os.path.basename(filename)
+        self.sockfd.send(('P '+ fname).encode())
+        data = self.sockfd.recv(1024).decode()
+        if data == 'y':
+
+            while 1:
+                wdata = fd.read(1024)
+                if not wdata:
+                    time.sleep(0.3)
+                    self.sockfd.send(b'##')
+                    break
+                self.sockfd.send(wdata)
+            fd.close()
+        
+    def do_guanbi(self):
+        self.sockfd.send('quit'.encode())
+        print('关闭')
+        sys.exit(0)
+
+
+def main():
+#    if len(sys,argv)<3:
+#        print('argv is error')
+#        sys.exit(1)
+    host = '127.0.0.1'
+    port = int(input('输入服务器端口：'))
+    addr = (host, port)
+#    buffersize = 1024
+    
+    sockfd = socket()
+    sockfd.connect(addr)
+    
+    # 创建客户端请求对象
+    tftp = Tftpclient(sockfd)
+
+    while 1:
+        print("""
+        --------命令选项----------
+        -------1.文件列表---------
+        -------2.下载文件---------
+        -------3.上传文件---------
+        -------4.退出服务---------
+        -------------------------
+        
+        """)
+        com = input('选择命令（1、2、3、4）：')
+        if com == '1':
+            tftp.do_liebiao()
+        elif com =='2':
+            filename = input('输入文件名:')
+            tftp.do_xiazai(filename)
+        elif com =='3':
+            tftp.do_shangchuan(filename)
+        elif com =='4':
+            tftp.do_guanbi()
+        else:
+            print('输入错误')
+            time.sleep(2)
+
+if __name__ == '__main__':
+    main()
+
---
+++ 183-TFTP_server_并发服务器.py
@@ -0,0 +1,127 @@
+#coding=utf-8
+from socket import socket
+import os, sys
+import signal
+import time
+import tkinter.filedialog as flog
+
+# 文件库位置
+file_path = flog.askdirectory(title = '选择数据目录')
+print(file_path)
+class Tftpserver():
+    def __init__(self,connfd):
+        self.connfd = connfd
+    
+    def do_list(self):
+        filelist = os.listdir(file_path)
+#        print(filelist)
+        if not filelist:
+            self.connfd.send('n'.encode())
+            return None
+        else:
+            self.connfd.send('y'.encode())
+        time.sleep(0.5)
+        files = ''
+        for fn in filelist:
+#            print(fn)
+            if fn[0] != '.' and os.path.isfile(file_path+'/'+fn):
+#                print(fn)
+                files = files + fn + ','
+#        print(files)
+        self.connfd.send(files.encode())
+        
+    def do_get(self,filename):
+        try:
+            fd = open(file_path + '/' + filename, 'rb')
+        except:
+            self.connfd.send(b'n')
+            return
+        self.connfd.send('y')
+        time.sleep(0.3)
+        for line in fd:
+            self.connfd.send(line)
+        fd.close()
+        time.sleep(0.2)
+        self.connfd.send(b'##')
+        print('发送成功')
+        
+    def do_put(self, filename):
+        try:
+            fd = open(file_path + '/' + filename, 'wb')
+        except:
+            self.connfd.send(b'n')
+            return
+        for line in fd:
+            self.connfd.send(line)
+        time.sleep(0.3)
+        self.connfd.send(b'##')
+        
+    def do_quit(self):
+        print('客户端退出')
+        sys.exit(0)
+    
+
+def main():
+#    if len(sys,argv)<3:
+#        print('argv is error')
+#        sys.exit(1)
+    host = '127.0.0.1'
+    port = 8899
+    addr = (host, port)
+    buffersize = 1024
+    
+    sockfd = socket()
+    sockfd.bind(addr)
+    sockfd.listen(5)
+    signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+    
+    while 1:
+        try:
+            connfd, addr = sockfd.accept()
+        except KeyboardInterrupt:
+            print('退出')
+            sockfd.close()
+            sys.exit(0)
+        except Exception as E:
+            print(E)
+            
+        print('客户端%s:%s登录'%addr)
+        pid = os.fork()
+        
+        if pid<0:
+            print('创建子进程失败')
+            connfd.close()
+        elif pid == 0:
+            sockfd.close()
+            
+            # 创建与客户端通信的对象
+            tftp = Tftpserver(connfd)
+            
+            while 1:
+                data = connfd.recv(buffersize).decode()
+                if data == 'list':
+                    tftp.do_list()
+                elif data == 'get':
+                    filename = data.split(' ')[-1]
+                    tftp.do_get(filename)
+                elif data == 'put':
+                    filename = data.split(' ')[-1]
+                    tftp.do_put(filename)
+                elif data == 'quit':
+                    tftp.do_quit()
+                
+        else:
+            connfd.close()
+            continue
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
